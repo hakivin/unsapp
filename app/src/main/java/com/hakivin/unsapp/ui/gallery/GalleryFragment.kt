e2041f210100155b2eacd7d3f34fe2343d247bd8
hakivin <hakivinamana@student.uns.ac.id>
2019-09-13 23:35:06 +0700

    Initial commit

---
+++ app/src/main/java/com/hakivin/unsapp/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,43 @@
+package com.hakivin.unsapp.ui.gallery
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.hakivin.unsapp.R
+
+class GalleryFragment : Fragment() {
+
+    private lateinit var galleryViewModel: GalleryViewModel
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        galleryViewModel =
+            ViewModelProviders.of(this).get(GalleryViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_gallery, container, false)
+        val webView : WebView = root.findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
+        }
+        webView.loadUrl("https://siakad.uns.ac.id")
+
+        return root
+    }
+}
